80a05a914f2bc999370a046a47d82f08b98697e0
Guigen.Wang <guigen.wang.cn@gmail.com>
2016-08-14 16:12:00 +0800

    DAO层继承HibernateDaoSupport类实现并用spring注入dao类中保存product对象

--- src/com/aaa/dao/ProductDao.java
+++ src/com/aaa/dao/ProductDao.java
@@ -1,17 +1,20 @@
 package com.aaa.dao;
 
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
 import com.aaa.domain.Product;
 
 /**
  * 商品管理的DAO的类
  */
-public class ProductDao {
+@SuppressWarnings("deprecation")
+public class ProductDao extends HibernateDaoSupport{
 	/**
 	 * DAO中的保存商品的方法 
 	 */
 	public void save(Product product) {
 		System.out.println("DAO中的save方法执行了");
-		
+		this.getHibernateTemplate().save(product);
 	}
 
 }
